3009307f4cfb615e8abc40bb3b515a7d09f99f98
김상준 <drbug2000@naver.com>
2022-05-18 14:29:12 +0000

    2022-05-18 update
    start Project 'Minesweeper'
    add readme file to make plan

---
+++ TEST/Minesweeper/test.js
@@ -0,0 +1,71 @@
+var tableSize =2;//게임판의 크기
+var minecounts =4; //지뢰의 개수
+
+
+var body = document.body;
+var table = document.createElement('table');
+//행 row 열 column / cell
+
+var underground =[];//실제 지뢰 위치
+var matrix =[];//보여지는 게임판
+//var turn='X';
+
+
+//본문 게임판 생성
+for(var i =0;i< tableSize; i++){
+	var rowTag = document.createElement('tr');
+		
+		matrix.push([]);
+	for(var j=0;j<tableSize; j++){
+		var cell=document.createElement('td');
+		cell.addEventListener('click',Fclick);
+		matrix[i].push(cell);
+		//cell.textContent = String(i)+String(j);
+		rowTag.appendChild(cell);
+		
+		
+	}
+	
+	table.appendChild(rowTag);
+		
+}
+body.appendChild(table);
+
+
+//cell 클릭시 일어나는 반응
+function Fclick(e){
+	console.log("click work");
+	console.log(e.target);//그줄 
+	//console.log(matrix.indexOf(e.target));
+	console.log(Findindex(e.target));
+	
+	
+	
+	
+}
+
+
+
+/*
+window.oncontextmenu = function ()
+{
+    showCustomMenu(); //이부분이 에러.... 없는 함수 라고 뜸
+    return false;     // cancel default menu
+}
+
+
+
+document.body.onclick = function (e) {
+    var isRightMB;
+    e = e || window.event;
+
+    if ("which" in e)  // Gecko (Firefox), WebKit (Safari/Chrome) & Opera
+        isRightMB = e.which == 3; 
+    else if ("button" in e)  // IE, Opera 
+        isRightMB = e.button == 2; 
+
+    alert("Right mouse button " + (isRightMB ? "" : " was not") + "clicked!");
+} 
+
+
+*/
